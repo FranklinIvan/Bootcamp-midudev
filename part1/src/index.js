93c12f4c9579325dfd0b3ead19a07aab50f9c388
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-01-20 16:35:35 -0500

    ejercicio js con react

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = ({ course }) => <h1>{course.name}</h1>
+const Content = ({ parts }) => {
+
+  const part1 = parts[0];
+  const part2 = parts[1];
+  const part3 = parts[2];
+
+  return (
+    <div>
+      <p>{part1.name} {part1.exercises}</p>
+      <p>{part2.name} {part2.exercises}</p>
+      <p>{part3.name} {part3.exercises}</p>
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+
+  const part1 = parts[0];
+  const part2 = parts[1];
+  const part3 = parts[2];
+
+  return <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
-);
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
