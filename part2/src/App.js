1a4ee4002b829545da5e081824ef08896988bc63
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-01-28 18:34:02 -0500

    siguiendo el bootcamp y la explicación de fetching y axios

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,111 +1,60 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
-// Ejercicios: 2.6.-2.10.
+// Explicación: viedeo 5 - Fetching y mutación de datos
 
 // components
-const Filter = ({ text, handleFilter, value }) => <>{text} <input onChange={handleFilter} value={value} /> </>
-
-const PersonForm = ({ submit, handleChangeName, handleChangeNumber, valueName, valueNumber, txt1, txt2 }) => {
-
+const Note = ({ title, body }) => {
   return (
-    <form onSubmit={submit}>
-      <p>
-        {txt1} <input onChange={handleChangeName} value={valueName} required />
-      </p>
-      <p>
-        {txt2} <input onChange={handleChangeNumber} value={valueNumber} required />
-      </p>
-      <>
-        <button>add</button>
-      </>
-    </form>
+    <li>
+      <p>{title}</p>
+      <small><time>{body}</time></small>
+    </li>
   )
 }
-
-const Person = ({ persons, filter }) => {
-
-  return (
-    <>
-      {
-        filter.length > 0
-          ? filter[0].name + ' ' + filter[0].number
-          : persons.map(person => <p key={person.name}> {person.name} {person.number}</p>)
-      }
-    </>
-  )
-}
-
-const INITIAL_PERSONS_STATE = [
-  { name: 'Arto Hellas', number: '040-123456' },
-  { name: 'Ada Lovelace', number: '39-44-5323523' },
-  { name: 'Dan Abramov', number: '12-43-234345' },
-  { name: 'Mary Poppendieck', number: '39-23-6423122' }
-]
 
 export default function App() {
-  const [persons, setPersons] = useState(INITIAL_PERSONS_STATE)
-  const [newContact, setnewContact] = useState({ name: '', number: '' })
-  const [filterName, setFilterName] = useState('')
+  const [notes, setNotes] = useState([]);
+  const [newNote, setNewNote] = useState('');
 
-  const handleChangeName = e => {
-    const copy = { ...newContact }
-    copy.name = e.target.value
-    setnewContact(copy);
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+    .then(json => {
+      const { data } = json
+      setNotes(data)
+    })
+  }, [])
+
+  const handleChange = (e) => {
+    setNewNote(e.target.value);
   }
 
-  const handleChangeNumber = e => {
-    const copy = { ...newContact }
-    copy.number = e.target.value
-    setnewContact(copy)
-  }
-
-  const handleFilter = e => {
-    setFilterName(e.target.value)
-  }
-
-  const handleSubmit = e => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-
-    const newPerson = {
-      name: newContact.name,
-      number: newContact.number
+    const newNoteToAddToState = {
+      id: notes.length + 1,
+      title: newNote,
+      body: newNote,
     }
 
-    for (let i = 0; i < persons.length; i++) {
-      if (persons[i].name === newPerson.name) {
-        alert(`${newPerson.name} already added to the phonebook`)
-        return
-      }
-    }
-
-    setPersons(persons.concat(newPerson))
+    setNotes(notes.concat(newNoteToAddToState));
+    setNewNote('');
   }
-
-  const filter = persons.filter(person => person.name.toLowerCase() === filterName.toLowerCase())
 
   return (
     <div>
-      <h2>Phonebook</h2>
-
-      <Filter text={'filter shown with'} handleFilter={handleFilter} value={filterName} />
-
-      <h3>Add a new</h3>
-
-      <PersonForm
-        submit={handleSubmit}
-        handleChangeName={handleChangeName}
-        valueName={newContact.name}
-        txt1={'name:'}
-        handleChangeNumber={handleChangeNumber}
-        valueNumber={newContact.number}
-        txt2={'number:'}
-      />
-
-      <h3>Numbers</h3>
-
-      <Person persons={persons} filter={filter} />
-
+      <h1>Notes</h1>
+      <ol>
+        {notes
+          .map(note =>
+            (<Note key={note.id} {...note} />)
+          )}
+      </ol>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleChange} value={newNote}></input>
+        <button>Create note</button>
+      </form>
     </div>
   )
 }
