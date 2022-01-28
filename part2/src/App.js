6fb0352bd1ecab8e6daaadac123788b42b0fa078
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-01-28 18:18:13 -0500

    ordenando los archivos de ejercicios en la carpeta part2

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,7 +1,111 @@
 import './App.css';
+import { useState } from 'react';
 
-function App() {
-  return (<h1>Hi there!</h1>);
+// Ejercicios: 2.6.-2.10.
+
+// components
+const Filter = ({ text, handleFilter, value }) => <>{text} <input onChange={handleFilter} value={value} /> </>
+
+const PersonForm = ({ submit, handleChangeName, handleChangeNumber, valueName, valueNumber, txt1, txt2 }) => {
+
+  return (
+    <form onSubmit={submit}>
+      <p>
+        {txt1} <input onChange={handleChangeName} value={valueName} required />
+      </p>
+      <p>
+        {txt2} <input onChange={handleChangeNumber} value={valueNumber} required />
+      </p>
+      <>
+        <button>add</button>
+      </>
+    </form>
+  )
 }
 
-export default App;
+const Person = ({ persons, filter }) => {
+
+  return (
+    <>
+      {
+        filter.length > 0
+          ? filter[0].name + ' ' + filter[0].number
+          : persons.map(person => <p key={person.name}> {person.name} {person.number}</p>)
+      }
+    </>
+  )
+}
+
+const INITIAL_PERSONS_STATE = [
+  { name: 'Arto Hellas', number: '040-123456' },
+  { name: 'Ada Lovelace', number: '39-44-5323523' },
+  { name: 'Dan Abramov', number: '12-43-234345' },
+  { name: 'Mary Poppendieck', number: '39-23-6423122' }
+]
+
+export default function App() {
+  const [persons, setPersons] = useState(INITIAL_PERSONS_STATE)
+  const [newContact, setnewContact] = useState({ name: '', number: '' })
+  const [filterName, setFilterName] = useState('')
+
+  const handleChangeName = e => {
+    const copy = { ...newContact }
+    copy.name = e.target.value
+    setnewContact(copy);
+  }
+
+  const handleChangeNumber = e => {
+    const copy = { ...newContact }
+    copy.number = e.target.value
+    setnewContact(copy)
+  }
+
+  const handleFilter = e => {
+    setFilterName(e.target.value)
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const newPerson = {
+      name: newContact.name,
+      number: newContact.number
+    }
+
+    for (let i = 0; i < persons.length; i++) {
+      if (persons[i].name === newPerson.name) {
+        alert(`${newPerson.name} already added to the phonebook`)
+        return
+      }
+    }
+
+    setPersons(persons.concat(newPerson))
+  }
+
+  const filter = persons.filter(person => person.name.toLowerCase() === filterName.toLowerCase())
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter text={'filter shown with'} handleFilter={handleFilter} value={filterName} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        submit={handleSubmit}
+        handleChangeName={handleChangeName}
+        valueName={newContact.name}
+        txt1={'name:'}
+        handleChangeNumber={handleChangeNumber}
+        valueNumber={newContact.number}
+        txt2={'number:'}
+      />
+
+      <h3>Numbers</h3>
+
+      <Person persons={persons} filter={filter} />
+
+    </div>
+  )
+}
---
+++ part2/src/App2.js
@@ -0,0 +1,62 @@
+import './App.css';
+import { useState } from 'react';
+
+// Ejercicios: 2.6.-2.10.
+
+// components
+const Note = ({ content, date, categories = [] }) => {
+  return (
+    <li>
+      <p>{content}</p>
+      <small><time>{date}</time></small>
+      {categories.map(category =>
+        <ul key={category}> <li>{category}</li></ul>
+      )}
+    </li>
+  )
+}
+
+export default function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
+
+  const handleChange = (e) => {
+    setNewNote(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newNoteToAddToState = {
+      id: notes.length + 1,
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5
+    }
+    console.log(newNoteToAddToState)
+    setNotes(notes.concat(newNoteToAddToState));
+    setNewNote('');
+  }
+
+  const handleShowAll = () => {
+    setShowAll(() => !showAll)
+  }
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <button onClick={handleShowAll}>{showAll ? 'show only important' : 'show all'}</button>
+      <ol>
+        {notes
+        .filter(note => showAll ? true : note.important === true)
+        .map(note =>
+          (<Note key={note.id} {...note} />)
+        )}
+      </ol>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={handleChange} value={newNote}></input>
+        <button>Create note</button>
+      </form>
+    </div>
+  )
+}
