3bdec0f1057df6467b3444271ea1bca146e16156
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-01-29 18:56:36 -0500

    agregando la parte 3 para desarrollar los ejercicos del bootcamp

---
+++ part3/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import './App.css';
+
+function App() {
+  return (<h1>Hi there!</h1>)
+}
+
+ReactDOM.render(
+    <App />,
+  document.getElementById('root')
+);
