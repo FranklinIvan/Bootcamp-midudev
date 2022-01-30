0fe302754d16619f05023831381405cc70a6a830
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-01-29 21:59:50 -0500

    método delete con nodejs

--- part3/index.js
+++ part3/index.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express()
 
-const notes = [
+let notes = [
     {
-      id: 1,
-      content: 'HTML is easy',
-      date: '2019-05-30T17:30:31.098Z',
-      important: true,
+        id: 1,
+        content: 'HTML is easy',
+        date: '2019-05-30T17:30:31.098Z',
+        important: true,
     },
     {
-      id: 2,
-      content: 'Browser can execute only JavaScript',
-      date: '2019-05-30T18:39:34.091Z',
-      important: false,
+        id: 2,
+        content: 'Browser can execute only JavaScript',
+        date: '2019-05-30T18:39:34.091Z',
+        important: false,
     },
     {
-      id: 3,
-      content: 'GET and POST are the most important methods of HTTP protocol',
-      date: '2019-05-30T19:20:14.298Z',
-      important: true,
+        id: 3,
+        content: 'GET and POST are the most important methods of HTTP protocol',
+        date: '2019-05-30T19:20:14.298Z',
+        important: true,
     },
 ]
 
@@ -26,16 +26,22 @@ app.get('/', (req, res) => {
     res.send('<h1>hi there</h1>')
 })
 
-app.get('/api/notes', (req,res) => {
+app.get('/api/notes', (req, res) => {
     res.json(notes)
 })
 
-app.get('/api/notes/:id', (req,res) => {
+app.get('/api/notes/:id', (req, res) => {
     const id = Number(req.params.id)
     const note = notes.find(note => note.id === id)
-    
-    if(note) res.json(note)
+
+    if (note) res.json(note)
     else res.status(404).end()
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+    const id = Number(req.params.id)
+    notes = notes.filter(note => note.id !== id);
+    res.status(204).end()
 })
 
 const PORT = 3001
