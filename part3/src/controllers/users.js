6fae2d7b1febe4a546a72edf7a531a120b6c7e47
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-02-05 21:17:13 -0500

    modifying the user controller to hash passwords

--- part3/src/controllers/users.js
+++ part3/src/controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt')
 const router = require('express').Router()
 const User = require('../models/User')
 
@@ -5,14 +6,23 @@ router.post('/', async (req, res) => {
   const { body } = req
   const { username, name, password } = body
 
+  console.log({ body })
+
+  const saltRounds = await bcrypt.genSalt(10)
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
   const newUser = new User({
     username,
     name,
-    passwordHash: password
+    passwordHash
   })
 
-  const savedUser = await newUser.save()
-  res.json(savedUser)
+  try {
+    const savedUser = await newUser.save()
+    res.status(201).json(savedUser)
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 module.exports = router
