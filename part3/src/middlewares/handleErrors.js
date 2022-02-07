b0587a1e6736ad2bd5b53c651be4aa1e35a8b4a8
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-02-07 16:29:15 -0500

    adding the validation to duplicate key error

--- part3/src/middlewares/handleErrors.js
+++ part3/src/middlewares/handleErrors.js
@@ -5,5 +5,10 @@ module.exports = (error, req, res, next) => {
       error: true,
       message: 'information used is malformed'
     })
+  } else if (error.code === 11000) {
+    res.status(400).json({
+      error: true,
+      message: 'the username is already taken'
+    })
   } else res.status(500).end()
 }
