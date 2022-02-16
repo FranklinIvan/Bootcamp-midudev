52e5c70c3d60b050c42ef0e708e7667c7410d6ad
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-02-15 21:59:09 -0500

    testing... frontend w/ cypress, login form, note form...

--- part2/cypress/integration/note_app.spec.js
+++ part2/cypress/integration/note_app.spec.js
@@ -11,11 +11,23 @@ describe('Note app', () => {
     cy.contains('show log in').click()
   })
 
-  it('login form can be opened and user can be logged', () => {
+  it('login form can be opened and user can be logged in', () => {
     cy.contains('show log in').click()
     cy.get('[placeholder="username"]').type('midudev')
-    cy.get('[placeholder="password"]').type('123')
+    cy.get('[placeholder="password"]').last().type('123')
     cy.get('#form-login-button').click()
-    cy.contains('show me')
+    cy.contains('create note')
+  })
+
+  describe('when a user logged in', () => {
+    beforeEach(() => {
+      cy.contains('show log in').click()
+      cy.get('[placeholder="username"]').type('midudev')
+      cy.get('[placeholder="password"]').last().type('123')
+      cy.get('#form-login-button').click()
+    })
+    it('a new note can be created', () => {
+      cy.contains('show me')
+    })
   })
 })
