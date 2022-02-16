e035d9c38ce95b55319db12e35461b906bb5791d
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-02-15 19:20:17 -0500

    implementing cypress to testing end to end

---
+++ part2/cypress/integration/note_app.spec.js
@@ -0,0 +1,21 @@
+describe('Note app', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000')
+    })
+
+    it('frontpage can be opened', () => {
+        cy.contains('Notes')
+    })
+
+    it('login form can be opened', () => {
+        cy.contains('show log in').click()
+    })
+
+    it('login form can be opened and user can be logged', () => {
+        cy.contains('show log in').click()
+        cy.get('[placeholder="username"]').type('midudev')
+        cy.get('[placeholder="password"]').type('123')
+        cy.get('#form-login-button').click()
+        cy.contains('show me')
+    })
+})
