4cdf3aa3b3057d4f64ac25cdd2d04d2f46b15a4d
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-02-16 20:33:06 -0500

    creating a testing endpoint

---
+++ part3/src/controllers/testing.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+const Note = require('../models/Note')
+const User = require('../models/User')
+
+router.get('/reset', async (_, res) => {
+  await Note.deleteMany({})
+  await User.deleteMany({})
+
+  res.status(204).end()
+})
+
+module.exports = router
