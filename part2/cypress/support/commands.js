9f21c128307994987e2b4f7a1dc7d45fb2bded07
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-02-17 20:28:59 -0500

    a new command to create  note

--- part2/cypress/support/commands.js
+++ part2/cypress/support/commands.js
@@ -1,9 +1,24 @@
+const webSite = 'http://localhost:3000'
+const api = 'http://localhost:3001/api'
+
 Cypress.Commands.add('login', ({username, password}) => {
-    cy.request('POST', 'http://localhost:3001/api/login', {
+    cy.request('POST', `${api}/login`, {
         username,
         password
       }).then(response => {
         window.localStorage.setItem('loggedNoteAppUser', JSON.stringify(response.body))
-        cy.visit('http://localhost:3000')
+        cy.visit(webSite)
       })
+})
+
+Cypress.Commands.add('createNote', ({content, important = false}) => {
+    cy.request({
+        method: 'POST',
+        url: `${api}/notes`,
+        body: {content, important},
+        headers: {
+          Authorization: `Bearer ${JSON.parse(localStorage.getItem('loggedNoteAppUser')).token}`
+        }
+    })
+    cy.visit(webSite)
 })
