7030ed3b0fe1ed4e61d5e000ec03e15a8c97a47f
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-02-22 17:39:29 -0500

    removing unnecessary lines of code

--- part4/src/store.js
+++ part4/src/store.js
@@ -1,8 +1,7 @@
 import { combineReducers, createStore } from 'redux'
-import { initNotes, noteReducer } from './reducers/noteReducer'
+import { noteReducer } from './reducers/noteReducer'
 import { filterReducer } from './reducers/filterReducer'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import notesServices from './services/notes'
 
 const reducer = combineReducers({
     notes: noteReducer,
