3697208dd0898599d32937ba92856dd3dd1ac76b
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-02-22 10:47:30 -0500

    refactoring store

--- part4/src/index.js
+++ part4/src/index.js
@@ -1,17 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import { combineReducers, createStore } from 'redux'
-import { noteReducer } from './reducers/noteReducer'
-import { filterReducer } from './reducers/filterReducer'
 import { Provider } from 'react-redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-
-const reducer = combineReducers({
-  notes: noteReducer,
-  filter: filterReducer
-})
-const store = createStore(reducer, composeWithDevTools())
+import { store } from './store'
 
 ReactDOM.render(
   <Provider store={store}>
