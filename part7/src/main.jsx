4e228139bba89c837e7e2dc2e6e0aa3e2648ef4b
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-03-05 11:09:13 -0500

    add files @vitejs react and implementing graphQL

---
+++ part7/src/main.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import {ApolloClient, HttpLink, InMemoryCache, gql} from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000'
+  })
+})
+
+const query = gql`
+  query {
+    allPersons {
+      id
+      name
+      phone
+      address {
+        street
+        city
+      }
+    }
+  }
+`
+
+client.query({ query })
+  .then(res => console.log(res.data))
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
