1f831092b20114528fbb220fdb61fcd180d65526
FranklinIvan <franklin.alvarado@utp.ac.pa>
2022-03-05 11:42:48 -0500

    creating a Persons component

---
+++ part7/src/components/Persons.jsx
@@ -0,0 +1,13 @@
+export const Persons = ({ persons }) => {
+  if (persons === null) return null
+  return (
+    <div>
+      <h2>Persons</h2>
+      {
+        persons.map(person => <div key={person.id}>
+          {person.name} {person.phone}
+        </div>)
+      }
+    </div>
+  )
+}
